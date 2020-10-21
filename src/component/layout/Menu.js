7dbc60be0c7513b2634fad8e0b96b8a9312cd8c6
komon <silwerbas@gmaill.com>
2020-10-21 10:22:30 +0700

    add school menu 20-10-2020

--- src/component/layout/Menu.js
+++ src/component/layout/Menu.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import SchoolMenu from '../widget/SchoolMenu'
 
 export default function Menu() {
     return (
         <nav id="sidebar">
-            
+            <ul className="list-unstyled components">
+                <SchoolMenu />
+            </ul>
         </nav>
 
     )
---
+++ src/component/widget/SchoolMenu.js
@@ -0,0 +1,54 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { Link, useHistory, useParams } from "react-router-dom";
+import { LoginContext } from '../../store/LoginProvider'
+import { UserContext } from '../../store/UserProvider'
+import { UrlContext } from '../../store/UrlProvider'
+import axios from 'axios'
+
+
+export default function SchoolMenu() {
+    const { login, setLogin } = useContext(LoginContext)
+    const { user, setUser } = useContext(UserContext)
+    const { url } = useContext(UrlContext)
+
+    const history = useHistory()
+
+    const [school, setSchool] = useState([])
+
+    const getSchool = async () => {
+        let res = await axios.get(url + 'api/techers/school', {
+            params: {
+                memberID: localStorage.userID
+            }
+        })
+
+        setSchool(res.data)
+    }
+
+    const Image = (props) => {
+        let picture;
+        if(props.picture != ""){
+            picture = <img  width="25" height="25" src={url+props.picture} />
+        }else{
+            picture = <img  width="25" height="25" src="../../assets/school.png" />
+        }
+        return picture
+    }
+
+    useEffect(() => {
+        getSchool()
+    }, [])
+
+    return (
+        <li>
+            <a href="#schoolSubmenu" data-toggle="collapse" aria-expanded="false" className="dropdown-toggle">School</a>
+            <ul className="collapse list-unstyled" id="schoolSubmenu">
+                {school.map((item, index) => (
+                    <li key={index}>
+                        <a href="#"><Image picture={item.schoolPicture}/> {item.schoolNameTH}</a>
+                    </li>
+                ))}
+            </ul>
+        </li>
+    )
+}
